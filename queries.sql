ed11a6827df6d36dc15b9a2ee71abb7fe8aea459
Samson Tush <samo4421@icloud.com>
2023-05-11 16:27:01 +0300

    Add queries

--- queries.sql
+++ queries.sql
@@ -24,3 +24,23 @@ WHERE name <> 'Gabumon';
 SELECT * FROM animals
 WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
 
+
+-- transactions
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT weight_change;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO weight_change;
+COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT * FROM animals ORDER BY escape_attempts DESC LIMIT 1;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals                   
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
