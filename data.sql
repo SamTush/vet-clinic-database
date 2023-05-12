7f78937b69ce19bfb49fe34e7ad3946e5c6c594c
Samson Tush <samo4421@icloud.com>
2023-05-12 22:55:08 +0300

    Add solution

--- data.sql
+++ data.sql
@@ -32,3 +32,46 @@ VALUES ('Blossom', '1998-10-13', 3, true, 17);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-5-14', 4, true, 22);
+
+-- inserting data into owners table 
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES('Pokemon');
+
+INSERT INTO species (name)
+VALUES('Digimon');
+
+UPDATE animals SET species_id
+= (CASE
+WHEN name LIKE '%mon' THEN 2
+ELSE 1
+END);
+
+UPDATE animals 
+SET owner_id = 
+    (CASE 
+        WHEN name LIKE 'Agumon' THEN 1
+        WHEN name IN ('Gabumon', 'Pikachu') THEN 2
+        WHEN name IN ('Devimon', 'Plantmon') THEN 3
+        WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN 4
+        WHEN name IN ('Angemon', 'Boarmon') THEN 5
+        ELSE NULL
+     END);
